b2fcefdcbdec5eba3bee8395aea7ced1f632cc07
messense <messense@icloud.com>
2021-09-18 11:05:33 +0800

    Upgrade sha-crypt to 0.3.1 to fix a panic

--- tests/test_crypt.py
+++ tests/test_crypt.py
@@ -10,3 +10,8 @@ def test_roundtrip():
 def test_verify_passlib():
     hashed = "$6$rounds=656000$Ykk6fjI2sU3/uprV$Z6yV/9Z741lfroSSzB9MwxSRnGeI9Z74hBkgNsHuojQJxZ9XjPkHg9jqqGLvWZ586wqnSSx5vrXZdhrMSZZE4/"  # NOQA
     assert sha512_crypt.verify("abc", hashed)
+
+
+def test_verify_fail():
+    hashed = "$6$rounds=656000$Ykk6fjI2sU3/uprV$Z6yV/9Z741lfroSSzB9MwxSRnGeI9Z74hBkgNsHuojQJxZ9XjPkHg9jqqGLvWZ586wqnSSx5vrXZdhrMSZZE4"  # NOQA
+    assert not sha512_crypt.verify("abc", hashed)
